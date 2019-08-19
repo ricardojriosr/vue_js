48ec3715a62556511254b62dcd165e62e6348398
belzona-rrios <rrios@belzonaboats.com>
2019-08-19 11:42:01 -0400

    Second Commit

---
+++ JS/lesson5.js
@@ -0,0 +1,54 @@
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: 'IMG/vmSocks-green.jpg',
+    inventory: 0,
+    inStock: false,
+    onSale: false,
+    cart: 0,
+    details: ["80% cotton","20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: 'IMG/vmSocks-green.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: 'IMG/vmSocks-blue.jpg'
+      }
+    ],
+    sizes: [
+      {
+          sizeId: "S",
+          sizeName: "Short"
+      },
+      {
+          sizeId: "M",
+          sizeName: "Medium"
+      },
+      {
+          sizeId: "L",
+          sizeName: "Large"
+      },
+      {
+          sizeId: "XL",
+          sizeName: "Extra Large"
+      }
+    ]
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1
+    },
+    updateProduct: function(variantImage) {
+        this.image = variantImage
+    },
+    removeFromCart: function() {
+      this.cart -= 1
+    }
+  }
+})
---
+++ lesson6.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Product App</title>
+    <link rel="stylesheet" href="CSS/style.css">
+  </head>
+  <body>
+
+    <div class="nav-bar"></div>
+
+    <div id="app">
+      <div class="product">
+
+        <div class="product-image">
+            <img v-bind:src="image">
+        </div>
+
+        <div class="product-info">
+            <h1>{{ product }} <span style="color:red" v-show="onSale">On Sale!</span></h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+
+            <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <h3>Colors:</h3>
+            <div v-for="variant in variants"
+            :key="variant.variantId"
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(variant.variantImage)">
+
+            </div>
+
+            <h3>Sizes:</h3>
+            <div v-for="size in sizes" :key="size.sizeId">
+                <p>{{ size.sizeName }}</p>
+            </div>
+
+            <button @click="addToCart"
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }">Add to cart</button>
+            <button @click="removeFromCart">Remove from cart</button>
+
+            <div class="cart">
+                <p>Cart({{cart}})</p>
+            </div>
+
+            </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- development version, includes helpful console warnings -->
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <script src="JS/lesson6.js"></script>
+  </body>
+</html>
